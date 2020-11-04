f74f786b392e2e7918532739d10a2d9645517f27
Josh Cashen <72808022+JCashen@users.noreply.github.com>
2020-11-04 09:49:31 +0000

    Update app.py

--- tutorial/app.py
+++ tutorial/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Bungle_11@35.242.174.199/game_reviews' # Set the connection string to connect to the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@35.242.174.199/game_reviews' # Set the connection string to connect to the database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db = SQLAlchemy(app) 
 
