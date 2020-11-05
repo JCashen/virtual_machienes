ec73d7ae3bef071e30ecbd4c47be8401d180af8f
Josh Cashen <72808022+JCashen@users.noreply.github.com>
2020-11-05 09:50:46 +0000

    Update __init__.py

--- todo2/application/__init__.py
+++ todo2/application/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Bungle_11@35.242.174.199/todo'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@35.242.174.199/todo'
 app.config['SECRET_KEY'] = 'SECRETKEY'
 
 from application import routes
